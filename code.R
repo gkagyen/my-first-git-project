a18fb2b0e514c57ee42999ea1aadf6f4c4989fa4
gkagyen <gkagyen@live.com>
2025-09-19 21:22:04 +0000

    added more code to the script

--- code.R
+++ code.R
@@ -4,6 +4,17 @@ data(penguins)
 head(penguins)
 summary(penguins)
 
+# Basic R scatter plot
 plot(penguins$bill_dep, penguins$bill_len,
-     xlab = 'Bill dength', ylab = 'Bill length',
+     xlab = 'Bill depth', ylab = 'Bill length',
      main = 'Penguins Scatter plot')
+
+# ggplot scatter plot
+library(ggplot2)
+ggplot(data=penguins, aes(
+  x=bill_dep, y=bill_len
+)) +
+  geom_point() +
+  labs(x='Bill depth', y='Bill length',
+       title = "Penguins scatter plot") +
+  theme_classic()
